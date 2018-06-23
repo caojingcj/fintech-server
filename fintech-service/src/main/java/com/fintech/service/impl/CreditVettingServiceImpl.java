c3e4bb9d166a17e77f01a1e9ac6ed74092086a9f
caojing <caojing@duodingfen.com>
2018-06-23 22:23:56 +0800

    自动信审服务

--- fintech-service/src/main/java/com/fintech/service/impl/CreditVettingServiceImpl.java
+++ fintech-service/src/main/java/com/fintech/service/impl/CreditVettingServiceImpl.java
@@ -1,16 +1,54 @@
 package com.fintech.service.impl;
 
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.fintech.dao.CustBaseinfoMapper;
+import com.fintech.dao.LogOrderMapper;
+import com.fintech.dao.OrderBaseinfoMapper;
 import com.fintech.enm.CreditVettingResultEnum;
+import com.fintech.enm.IdentityStatusEnum;
+import com.fintech.enm.OrderOperationEnum;
+import com.fintech.enm.OrderStatusEnum;
+import com.fintech.model.CustBaseinfo;
+import com.fintech.model.LogOrder;
+import com.fintech.model.OrderBaseinfo;
 import com.fintech.service.CreditVettingService;
+import com.fintech.util.IDCardUtil;
 
 public class CreditVettingServiceImpl implements CreditVettingService {
+    
+    @Autowired
+    private OrderBaseinfoMapper orderBaseinfoMapper;
+    
+    @Autowired
+    private CustBaseinfoMapper custBaseinfoMapper;
+    
+    @Autowired
+    private LogOrderMapper logOrderMapper;
 
     @Override
     public CreditVettingResultEnum creditVetting(String orderId) {
         // 拒绝 - 年龄18岁以下
-        
+        OrderBaseinfo orderBaseInfo = orderBaseinfoMapper.selectByPrimaryKey(orderId);
+        String custId = orderBaseInfo.getCustIdCardNo();
+        int age = IDCardUtil.getAgeByIdCard(custId);
+        if (age < 18) {
+            logOrder(orderId, CreditVettingResultEnum.拒绝.getValue(), "拒绝 - 年龄18岁以下");
+            return CreditVettingResultEnum.拒绝;
+        }
         // 拒绝 - 年龄60岁以上
+        if (age > 60) {
+            logOrder(orderId, CreditVettingResultEnum.拒绝.getValue(), "拒绝 - 年龄60岁以上");
+            return CreditVettingResultEnum.拒绝;
+        }
         // 拒绝 - 身份证有效期失效
+        CustBaseinfo custBaseInfo = custBaseinfoMapper.selectByPrimaryKey(orderBaseInfo.getCustCellphone());
+        if (custBaseInfo.getIdentityStatus().equals(IdentityStatusEnum.未认证.getValue()) || custBaseInfo.getIdentityStatus().equals(IdentityStatusEnum.已过期.getValue())) {
+            logOrder(orderId, CreditVettingResultEnum.拒绝.getValue(), "拒绝 - 身份证有效期失效");
+            return CreditVettingResultEnum.拒绝;
+        }
         // 拒绝 - 学生（年龄18-21岁；学历 专科/本科/硕士及以上）
         // 拒绝 - 同一申请人申请超过3笔
         // 拒绝 - 同一申请人借款金额高于50w
@@ -28,6 +66,22 @@ public class CreditVettingServiceImpl implements CreditVettingService {
         // 通过(且) - 互通定义，主叫和被叫都有记录
         // 通过(且) - 通讯录号码>=10
         return CreditVettingResultEnum.拒绝;
+    }
+    
+    private void logOrder(String orderId, String result, String note) {
+        // 新增订单操作日志
+        LogOrder log = new LogOrder();
+        log.setOrderId(orderId);
+        log.setOrderOperation(OrderOperationEnum.审批.getValue());
+        if (result.equals(CreditVettingResultEnum.通过.getValue())) {
+            log.setOrderStatus(OrderStatusEnum.分期还款中.getValue());
+        }
+        if (result.equals(CreditVettingResultEnum.拒绝.getValue())) {
+            log.setOrderNote(note);
+            log.setOrderStatus(OrderStatusEnum.审批拒绝.getValue());
+        }
+        log.setCreateTime(new Date());
+        logOrderMapper.insert(log);
     }
 
 }
