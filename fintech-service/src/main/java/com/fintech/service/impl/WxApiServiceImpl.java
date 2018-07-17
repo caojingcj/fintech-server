6a69b2c54abfc906b835a9a37a072ac00f336d26
qierkang <xyqierkang@163.com>
2018-07-18 00:08:54 +0800

    2018年7月18日 00:08:51 尔康

--- fintech-service/src/main/java/com/fintech/service/impl/WxApiServiceImpl.java
+++ fintech-service/src/main/java/com/fintech/service/impl/WxApiServiceImpl.java
@@ -288,13 +288,10 @@ public class WxApiServiceImpl implements WxApiService {
 	    public String pushImgAndTextMessage(DataXmlPackageDo dataXmlPackageDO, CompanyBaseinfo baseinfo) throws Exception {
 	        StringBuffer stringBuffer = new StringBuffer();
 	        stringBuffer.append("<xml>");
-	        // 因为是要往客户微信上发送消息，所以formUserName应该是微信公众号的ID，ToUserName 才是用户的OpenId
-	        // 接收方帐号（收到的OpenID）
+	        // 因为是要往客户微信上发送消息，所以formUserName应该是微信公众号的ID，ToUserName 才是用户的OpenId 接收方帐号（收到的OpenID）
 	        stringBuffer.append("<ToUserName>" + dataXmlPackageDO.getFromUserName() + "</ToUserName>");
 	        // 开发者微信号
 	        stringBuffer.append("<FromUserName>" + dataXmlPackageDO.getToUserName() + "</FromUserName>");
-//	        stringBuffer.append("<ToUserName>" + dataXmlPackageDO.getToUserName() + "</ToUserName>");
-//	        stringBuffer.append("<FromUserName>" + dataXmlPackageDO.getFromUserName() + "</FromUserName>");
 	        stringBuffer.append("<CreateTime>" + System.currentTimeMillis() + "</CreateTime>");
 	        if (baseinfo == null || !"1".equals(baseinfo.getCompanyStatus())) {
 	            String messageContent = "商户是禁用或还未开通，请联系运营人员！";
@@ -312,12 +309,12 @@ public class WxApiServiceImpl implements WxApiService {
 	            stringBuffer.append("<PicUrl>https://www.duodingfen.com/fintech-manage/img/icon/logo.png</PicUrl>");
 	            stringBuffer.append("<Url>"+applyUrl+"</Url>");
 	            stringBuffer.append("</item>");
-//	            stringBuffer.append("<item>");
-//	            stringBuffer.append("<Title>" + baseinfo.getCompanyName() + "</Title>");
-//	            stringBuffer.append("<Description>点击申请</Description>");
-//	            stringBuffer.append("<PicUrl>https://www.duodingfen.com/fintech-manage/img/icon/logo.png</PicUrl>");
-//	            stringBuffer.append("<Url>" + applyUrl + "</Url>");
-//	            stringBuffer.append("</item>");
+	            stringBuffer.append("<item>");
+	            stringBuffer.append("<Title>" + baseinfo.getCompanyName() + "</Title>");
+	            stringBuffer.append("<Description>点击申请</Description>");
+	            stringBuffer.append("<PicUrl>https://www.duodingfen.com/fintech-manage/img/icon/logo.png</PicUrl>");
+	            stringBuffer.append("<Url>" + applyUrl + "</Url>");
+	            stringBuffer.append("</item>");
 	            stringBuffer.append("</Articles>");
 	        }
 	        stringBuffer.append("</xml>");
